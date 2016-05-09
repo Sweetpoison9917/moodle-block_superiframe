7b2378fb77cd8ae7deb6a26fa557ea69c70a6262
Shubhendra Doiphode <s.doiphode@qmul.ac.uk>
2016-05-09 14:58:03 +0100

    Added updates from week 5

--- renderer.php
+++ renderer.php
@@ -17,6 +17,144 @@ class block_superiframe_renderer extends plugin_renderer_base {
    }
     //Here we aggregate all the pieces of content of the view page and displays them
     function display_view_page($url, $width, $height){
-    
-    }
+		global $USER;
+		
+		//start output to browser
+		//always display the header first.
+		echo $this->output->header();
+		
+		//lets spice this top part up a bit soon
+		echo '<br>' . fullname($USER);
+		echo '<br>' . $this->output->user_picture($USER);
+		
+		//add the links to specify iframe size
+		echo $this->fetch_size_links();
+		
+		//prepare our frame
+		$iframe_atts = array();
+		$iframe_atts ['src']=$url;
+		$iframe_atts ['height']=$height;
+		$iframe_atts ['width']=$width;
+		$iframe_atts ['class']='block_superiframe_iframe';
+		$iframe = html_writer::tag('iframe','',$iframe_atts);
+		echo '<br>' . $iframe;
+		
+		//send footer out to browser
+		//never forget to do this ...!
+		echo $this->output->footer();
+		    
+    }	
+	
+	function fetch_size_links(){
+		$customlink = new moodle_url('/blocks/superiframe/view.php',array('size'=>'custom'));
+		$smalllink = new moodle_url('/blocks/superiframe/view.php',array('size'=>'small'));
+		$mediumlink = new moodle_url('/blocks/superiframe/view.php',array('size'=>'medium'));
+		$biglink = new moodle_url('/blocks/superiframe/view.php',array('size'=>'big'));
+		
+		$links = array();
+		$links[] = html_writer::link($customlink, get_string('custom','block_superiframe'), array('class'=>'block_superiframe_size_custom'));
+		$links[] = html_writer::link($smalllink, get_string('small','block_superiframe'), array('class'=>'block_superiframe_size_small'));
+		$links[] = html_writer::link($mediumlink, get_string('medium','block_superiframe'), array('class'=>'block_superiframe_size_medium'));
+		$links[] = html_writer::link($biglink, get_string('big','block_superiframe'), array('class'=>'block_superiframe_size_big'));
+		
+		return html_writer::div(implode(' ',$links),'block_superiframe_sizes');
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- view.php
+++ view.php
@@ -3,6 +3,15 @@
 require('../../config.php');
 require_once('../../lib/moodlelib.php');
 
+
+
+
+//fetch the size of the iframe
+$size = optional_param('size','medium',PARAM_TEXT);
+
+//set the url of the $PAGE
+//note we do this before require_login preferably
+$PAGE->set_url('/blocks/superiframe/view.php',array('size'=>$size));
 require_login();
 
 
@@ -12,41 +21,36 @@ $def_config = get_config('block_superiframe');
 
 $usercontext = context_user::instance($USER->id);
 $PAGE->set_course($COURSE);
-$PAGE->set_url('/blocks/superiframe/view.php');
+
 $PAGE->set_heading($SITE->fullname);
 $PAGE->set_pagelayout($def_config->pagelayout);
 $PAGE->set_title(get_string('pluginname', 'block_superiframe'));
 $PAGE->navbar->add(get_string('pluginname', 'block_superiframe'));
 
+$renderer=$PAGE->get_renderer('block_superiframe');
 
-// start output to browser
-echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('pluginname', 'block_superiframe'),5);
+switch($size){
+	case 'custom':
+		$width = $def_config->width;
+		$height = $def_config->height;
+		break;
+	case 'small':
+		$width = 360;
+		$height = 240;
+		break;
+	case 'medium':
+		$width = 800;
+		$height = 600;
+		break;
+	case 'big':
+		$width = 1024;
+		$height = 768;
+		break;
+	
+}
 
-/* User's fullname
-This is the function that Moodle prepares for us and this will handle display of user's name correctly.
-This is function is what Moodle provides for us from moodlelib.php (required above)
-*/
-echo '<br>' . fullname($USER) . '<br>';
-/* Output user's picture
-We are using global $OUTPUT variable which is instance of core renderer, that has a nice function called user_picture, which given
-the $USER object, will return the HTML that uses picture. and we are outputing that here straight away.
-It is possible to put size here big or small.
-*/
-echo '<br>' . $OUTPUT->user_picture($USER) . '<br>';
+$renderer->display_view_page($def_config->url, $width, $height);
 
 
-$src = $def_config->url;
-
-
-$height = $def_config->height;
-$width = $def_config->width;
-
-
-// Some content goes here
-echo "<iframe src='$src' height='$height' width='$width' style='border:0'></iframe>";
-
-//send footer out to browser
-echo $OUTPUT->footer();
 return;
 	
